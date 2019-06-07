782167e161e7631b065a191709c6c69d856bd36f
Shaneen Muhammad <mshaneen.sm@gmail.com>
2019-06-07 17:35:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  some_array = ["bananas", "apples", "lemons", "mint"]
 end
 
 def add_element_to_start_of_array(array, element)
